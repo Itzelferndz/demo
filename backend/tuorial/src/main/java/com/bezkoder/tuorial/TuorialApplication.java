55e82f4140972ee33e02663bd98f4b03e1a47961
itzel Fernandez <itzelferndz@gmail.com>
2021-02-04 14:11:14 -0600

    1st push

---
+++ backend/tuorial/src/main/java/com/bezkoder/tuorial/TuorialApplication.java
@@ -0,0 +1,13 @@
+package com.bezkoder.tuorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuorialApplication.class, args);
+	}
+
+}
---
+++ backend/tuorial/src/test/java/com/bezkoder/tuorial/TuorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezkoder.tuorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
